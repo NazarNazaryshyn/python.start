8f439b94638e21e0afae82529aaf8f732543a47c
NazarNazaryshyn <94219552+NazarNazaryshyn@users.noreply.github.com>
2021-11-13 16:57:05 +0200

    Add files via upload

---
+++ python.lesson1.py
@@ -0,0 +1,63 @@
+# Task 1
+
+
+print('Hello world')
+
+
+# Task 2
+
+
+print('My name is Nazar')
+print('Any message')
+print('Text')
+
+
+# Task 3
+
+
+width = int(input('Введіть ширину : '))
+height = int(input('Введіть висоту : '))
+
+S = width * height
+
+print('Площа прямокутника = ', end='')
+print(S)
+
+
+# Task 4
+
+
+x = int(input('Введіть перше число : '))
+y = int(input('Введіть друге число : '))
+
+print('Сума = ', end='')
+print(x + y)
+print('Різниця = ', end='')
+print(x - y)
+print('Добуток = ', end='')
+print(x * y)
+print('Ділення = ', end='')
+print(x / y)
+
+
+# Task 5
+
+
+R = int(input('Введіть радіус кола : '))
+
+print('Діаметр кола = ', end='')
+print(2 * R)
+
+import math
+P = math.pi
+print('Довжина кола  = ', end='')
+print(2 * P * R)
+
+print('Площа круга  = ', end='')
+print(2 * P * R**2)
+
+
+
+
+
+
